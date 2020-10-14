3f4c9724a5da54ce306365bd29c45203061b8fd8
Mateusz Lata <mateuszlata2@gmail.com>
2020-10-14 22:11:59 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleEraNames } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rotating-cards-gallery';
+  cities = [
+    { 
+      title: 'Kraków', 
+      subtitle: 'City of the Kings',
+      imgUrl: 'assets/images/krakow.jpg',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id tortor quis felis egestas viverra. Sed dictum lectus et urna aliquet luctus. Nunc commodo nec sapien eu finibus. Aliquam facilisis finibus est ut hendrerit. Vivamus sollicitudin rhoncus massa, id lobortis est. Curabitur mollis urna vitae ultrices molestie.'
+    },
+    { 
+      title: 'Warszawa', 
+      subtitle: 'The Capital of Poland', 
+      imgUrl: 'assets/images/warszawa.jpg',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id tortor quis felis egestas viverra. Sed dictum lectus et urna aliquet luctus. Nunc commodo nec sapien eu finibus. Aliquam facilisis finibus est ut hendrerit. Vivamus sollicitudin rhoncus massa, id lobortis est. Curabitur mollis urna vitae ultrices molestie.'
+    },
+    { 
+      title: 'Gdańsk', 
+      subtitle: 'Beautiful city with maritime climate', 
+      imgUrl: 'assets/images/gdansk.jpg',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id tortor quis felis egestas viverra. Sed dictum lectus et urna aliquet luctus. Nunc commodo nec sapien eu finibus. Aliquam facilisis finibus est ut hendrerit. Vivamus sollicitudin rhoncus massa, id lobortis est. Curabitur mollis urna vitae ultrices molestie.'
+    },
+  ]
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() title = '';
+  @Input() subtitle = '';
+  @Input() imgUrl = '';
+  @Input() content= '';
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
